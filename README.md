0a15f026cff464ce503816147dccffc257821671
ilham <radithya.ilham@gmail.com>
2023-01-27 19:17:05 +0700

    markdown edited

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-hallo ini miniproject untuk merchant
+### exshop sample
+1 . User registrasi melalui email, satu email hanya bisa digunakan untuk satu akun (filter middleware)
+2 . User login kemudian mendapatkan token (JWT)
+3 . Token digunakan untuk melihat profile, create new product, show product by ID user
+
+### ERD
+
+### Diagram
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -2,10 +2,10 @@ const middleware = require('../middleware')
 const controller = require('../controllers/auth.controller')
 
 module.exports= (app) =>{
-    app.use(function(req, res, next){
-        res.header(
-            'Access-Control-Allow-Headers',
-            'authorization, Origin, Content-Type, Accept'
+  app.use(function(req, res, next){
+    res.header(
+      'Access-Control-Allow-Headers',
+        'authorization, Origin, Content-Type, Accept'
         )
         next()
     })
